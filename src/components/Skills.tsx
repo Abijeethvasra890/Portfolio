f59e8a0db2ef2a58ad4fadde09142649f41e64da
Abijeeth Vasra <abijeeth.vasra@bounteous.com>
2024-06-19 10:24:48 +0530

    vercel speed analytics

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,16 +6,16 @@ const Skills = () => {
              bg-opacity-40 border-2 border-gray-500 transform transition-transform duration-500 hover:scale-105 hover:shadow-3xl'>
             <h4 className='text-white mb-5'>Tech Stack</h4>
             <div className='flex justify-around'>
-              <img src='htmllogo.png' className='w-8 h-8'/>
-              <img src='csslogo.png' className='w-8 h-8'/>
-              <img src='tailwindlogo.png' className='w-8 h-8'/>
-              <img src='reduxlogo.png' className='w-8 h-8'/>
+              <img src='htmllogo.png' className='w-12 h-12'/>
+              <img src='csslogo.png' className='w-12 h-12'/>
+              <img src='tailwindlogo.png' className='w-12 h-12'/>
+              <img src='reduxlogo.png' className='w-12 h-12'/>
             </div>
             <div className='flex justify-around mt-3'>
-              <img src='jslogo.png' className='w-8 h-8'/>
-              <img src='tslogo.jpg' className='w-8 h-8'/>
-              <img src='postmanlogo.png' className='w-12 h-8'/>
-              <img src='reactlogo.png' className='w-8 h-8'/>
+              <img src='jslogo.png' className='w-12 h-12'/>
+              <img src='tslogo.jpg' className='w-12 h-12'/>
+              <img src='postmanlogo.png' className='w-12 h-12'/>
+              <img src='reactlogo.png' className='w-12 h-12'/>
             </div>
             <div className='flex justify-around mt-3'>
               <img src='tflogo.png' className='w-8 h-8'/>
