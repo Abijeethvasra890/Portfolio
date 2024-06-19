80bddc5f5a782dd31d64894fb4e5216b48dc1782
Abijeeth Vasra <abijeeth.vasra@bounteous.com>
2024-06-19 12:11:57 +0530

    astro fix

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,22 +6,22 @@ const Skills = () => {
              bg-opacity-40 border-2 border-gray-500 transform transition-transform duration-500 hover:scale-105 hover:shadow-3xl'>
             <h4 className='text-white mb-5'>Tech Stack</h4>
             <div className='flex justify-around'>
-              <img src='htmllogo.png' className='w-12 h-12'/>
-              <img src='csslogo.png' className='w-12 h-12'/>
-              <img src='tailwindlogo.png' className='w-12 h-12'/>
-              <img src='reduxlogo.png' className='w-12 h-12'/>
+              <img src='htmllogo.png' className='w-12 h-12 transform transition-transform duration-300 hover:scale-110'/>
+              <img src='csslogo.png' className='w-12 h-12 transform transition-transform duration-300 hover:scale-110'/>
+              <img src='tailwindlogo.png' className='w-12 h-12 transform transition-transform duration-300 hover:scale-110'/>
+              <img src='reduxlogo.png' className='w-12 h-12 transform transition-transform duration-300 hover:scale-110'/>
             </div>
             <div className='flex justify-around mt-3'>
-              <img src='jslogo.png' className='w-12 h-12'/>
-              <img src='tslogo.jpg' className='w-12 h-12'/>
-              <img src='postmanlogo.png' className='w-12 h-12'/>
-              <img src='reactlogo.png' className='w-12 h-12'/>
+              <img src='jslogo.png' className='w-12 h-12 transform transition-transform duration-300 hover:scale-110'/>
+              <img src='tslogo.jpg' className='w-12 h-12 transform transition-transform duration-300 hover:scale-110'/>
+              <img src='postmanlogo.png' className='w-12 h-12 transform transition-transform duration-300 hover:scale-110'/>
+              <img src='reactlogo.png' className='w-12 h-12 transform transition-transform duration-300 hover:scale-110'/>
             </div>
             <div className='flex justify-around mt-3'>
-              <img src='tflogo.png' className='w-8 h-8'/>
-              <img src='javalogo.png' className='w-8 h-8'/>
-              <img src='firebaselogo.jpg' className='w-8 h-8'/>
-              <img src='magentologo.png' className='w-8 h-8'/>
+              <img src='tflogo.png' className='w-12 h-12 transform transition-transform duration-300 hover:scale-110'/>
+              <img src='javalogo.png' className='w-12 h-12 transform transition-transform duration-300 hover:scale-110'/>
+              <img src='firebaselogo.jpg' className='w-12 h-12 transform transition-transform duration-300 hover:scale-110'/>
+              <img src='magentologo.png' className='w-12 h-12 transform transition-transform duration-300 hover:scale-110'/>
             </div>
             
     </div>
