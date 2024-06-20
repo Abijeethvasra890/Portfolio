dadbd9f1de4c87c2ca6f9c453849ddabb334ea79
Abijeeth Vasra <abijeeth.vasra@bounteous.com>
2024-06-20 17:51:51 +0530

    speed and web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,4 +9,10 @@ export default defineConfig({
     react(),
     tailwind(),
   ],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
